899f31f7b5758133e5a1288a569a091d6fceeb7c
enzoquarelo <enzo.quarelo@gmail.com>
2023-09-06 08:55:25 -0300

    START API

---
+++ senai.inlock.webApi/senai.inlock.webApi/Domains/JogoDomain.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace senai.inlock.webApi.Domains
+{
+    public class JogoDomain
+    {
+        public int IdJogo { get; set; }
+
+        [Required(ErrorMessage = "O nome do jogo é obrigatório")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "É necessário uma descrição para o jogo")]
+        public string? Descricao { get; set; }
+
+
+        public int IdGenero { get; set; }
+
+        public GeneroDomain? Genero { get; set; }
+    }
+}
---
+++ senai.inlock.webApi/senai.inlock.webApi/Domains/TipoDeUsuario.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace senai.inlock.webApi.Domains
+{
+    public class TipoDeUsuario
+    {
+        public int IdTipoDeUsuario { get; set; }
+
+        [Required(ErrorMessage = "O campo titulo é obrigatório!")]
+        public string Titulo { get; set; }
+
+    }
+}
